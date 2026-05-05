dc0663d4bbfbb42c0c421ffb956f13b9a21babf6
Papan Banik <papanbanik58@gmail.com>
2026-05-05 21:19:10 +0600

    updated readme

--- app/addTopic/page.tsx
+++ app/addTopic/page.tsx
@@ -13,7 +13,7 @@ const Page = () => {
         return; 
       }
       try{
-      const res=await fetch('http://localhost:3000/api/topics',{
+      const res=await fetch('/api/topics',{
         method:"POST",
         headers:{"Content-type":"application/json"},
         body:JSON.stringify({title,description}),
